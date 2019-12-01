a271b04c858d0db717dcc87eab6fecb6e3b12cdc
Greg Holmes <git@greg.holmes.name>
2019-12-01 20:39:05 +0000

    Add day 1

---
+++ 01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"aoc-2019/common"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func getFuelForModule(mass int64) int64 {
+	return (mass / 3) - 2
+}
+
+func getAnswer(input []string) (int64, int64) {
+	var answer int64 = 0
+	var recursiveAnswer int64 = 0
+	for _, line := range input {
+		mass, err := strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			log.Panicf("Input not an int")
+		}
+		fuelForModule := getFuelForModule(mass)
+		answer += fuelForModule
+		for fuelForModule > 0 {
+			recursiveAnswer += fuelForModule
+			fuelForModule = getFuelForModule(fuelForModule)
+		}
+	}
+	return answer, recursiveAnswer
+}
+
+func main() {
+	input := common.ReadInput("01/input.txt")
+	part1, part2 := getAnswer(input)
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
---
+++ common/reader.go
@@ -0,0 +1,15 @@
+package common
+
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func ReadInput(path string) []string {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Panicf("Error reading input: %s", err)
+	}
+	return strings.Split(strings.Replace(string(data), "\r\n", "\n", -1), "\n")
+}
